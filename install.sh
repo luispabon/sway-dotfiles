fbd7305ae87b5348455bcf5f90df3a1f94a7cb78
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-05-23 11:18:02 +0100

    Add kanshi - config not yet

--- install.sh
+++ install.sh
@@ -2,11 +2,24 @@
 start_green="\033[92m"
 end_green="\033[39m"
 
+current=${PWD}
+
 echo -e "\n${start_green} Installing dependencies and fonts...${end_green}"
+
+sudo add-apt-repository ppa:samoilov-lex/sway
 
 sudo apt install \
     brightnessctl \
-    j4-dmenu-desktop
+    j4-dmenu-desktop \
+    grim \
+    slurp \
+    sway \
+    sway-backgrounds \
+    swaybg \
+    swayidle \
+    swaylock \
+    xdg-desktop-portal-wlr \
+    wl-clipboard
 
 cp monaco.ttf ~/.local/share/fonts
 
@@ -24,9 +37,14 @@ fi
 
 echo -e "\n${start_green} Linking sway config folders into ~/.config... ${end_green}"
 
-folders_to_linky=("sway" "i3status" "waybar" )
+folders_to_linky=("sway" "i3status" "waybar" "kanshi")
 for folder in ${folders_to_linky[@]}; do
     if [[ ! -e "${HOME}/.config/${folder}" ]]; then
         ln -s ${PWD}/${folder}/ "${HOME}/.config/${folder}"
     fi
 done
+
+# Install kanshi
+cd builds/kanshi
+make build copy-bin
+cd ${current}
