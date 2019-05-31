273b2f396429a884cafa0e929e1e6a5fbd75a535
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-05-31 18:56:24 +0100

    Add exclusivew install script

---
+++ install_exclusive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt install \
+    connman \
+    connman-gtk \
+    cron \
+    snapd \
+    curl \
+    ca-certificates \
+    yaru-theme-icon \
+    pulseaudio \
+    xwayland \
+    libjsoncpp1 \
+    libsigc++ \
+    libgtkmm-3.0-1v5 \
+    pavucontrol \
+    ubuntu-restricted-extras
+
+sudo apt install --no-install-recommends \
+    gnome-terminal \
+    nautilus \
+    libreoffice
+
+folders_to_linky=("gtk-3.0")
+for folder in ${folders_to_linky[@]}; do
+    if [[ ! -e "${HOME}/.config/${folder}" ]]; then
+        ln -s ${PWD}/${folder}/ "${HOME}/.config/${folder}"
+    fi
+done
