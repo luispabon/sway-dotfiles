45174edd1fba278118f3507c0d9d0913763660bd
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-06-04 14:05:22 +0100

    Clean up brightness change script

--- brightness-notification.sh
+++ brightness-notification.sh
@@ -8,22 +8,10 @@ PCT=$(echo $BRIGHTNESS $MAX_BRIGHTNESS  | awk '{printf "%4.0f\n",($1/$2)*100}' |
 # Round the brightness percentage:
 LC_ALL=C
 
-# Kill previous notifications so the brightness notifications don't pile up:
-# NOTIFD=`cat /usr/share/dbus-1/services/org.freedesktop.Notifications.service | grep Exec | cut -d = -f 2`
-# killall -q $NOTIFD
-
-if [[ $LANG == "de_DE.UTF-8" ]]; then
-        BRIGHTNESS="Helligkeit"
-elif [[ $LANG == "ga_IE.UTF-8" ]]; then
-        BRIGHTNESS="Gile"
-else
-        BRIGHTNESS="Brightness"
-fi
-
 # Send the notification with the icon:
 notify-send.sh "  " \
     --replace-file=/tmp/brightness-notification \
     -t 2000 \
     --icon ~/.config/icons/brightness-icon.png \
     -h int:value:${PCT} \
-    -h string:x-canonical-private-synchronous:anything
+    -h string:synchronous:brightness-change
