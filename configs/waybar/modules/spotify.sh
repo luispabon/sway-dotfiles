445293fefa00a5f061ead6b788624c228b9a7a35
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-06-20 23:11:55 +0100

    Spotify script I could never make work

--- configs/waybar/modules/spotify.sh
+++ configs/waybar/modules/spotify.sh
@@ -3,15 +3,15 @@
 class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
 icon=""
 
-if [[ $class == "playing" ]]; then
+if [[ "${class}" == "playing" ]]; then
   info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
   if [[ ${#info} > 40 ]]; then
     info=$(echo $info | cut -c1-40)"..."
   fi
   text=$info" "$icon
-elif [[ $class == "paused" ]]; then
+elif [[ "${class}" == "paused" ]]; then
   text=$icon
-elif [[ $class == "stopped" ]]; then
+elif [[ "${class}" == "stopped" ]]; then
   text=""
 fi
 
