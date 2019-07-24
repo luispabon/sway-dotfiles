184a499978ba6cacf2eee39ff2ae070d41961530
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-07-24 17:47:06 +0100

    Cleanup weather script and improve location detection

--- configs/waybar/modules/weather.sh
+++ configs/waybar/modules/weather.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# wttr.in's location service is a bit shit
+location=$1
+if [[ -z "$1" ]]; then
+    ifconfigco=`curl -sS ifconfig.co/json`
+    city=`echo $ifconfigco | jq ".city"`
+    country_code=`echo $ifconfigco | jq ".country_iso"`
+
+    location=`echo $city,$country_code | sed --expression "s/\"//g"`
+fi
+
+# Set up caching to avoid tons of reqs to wttr
 cachedir=~/.cache/rbn
-cachefile=${0##*/}-$1
+cachefile=${0##*/}-$location
 
 if [ ! -d $cachedir ]; then
     mkdir -p $cachedir
@@ -18,7 +29,7 @@ IFS=$'\n'
 
 cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
 if [ $cacheage -gt 1740 ] || [ ! -s $cachedir/$cachefile ]; then
-    data=($(curl -s https://en.wttr.in/$1\?0qnT 2>&1))
+    data=($(curl -s https://en.wttr.in/$location\?0qnT 2>&1))
     echo ${data[0]} | cut -f1 -d, > $cachedir/$cachefile
     echo ${data[1]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
     echo ${data[2]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
@@ -34,44 +45,45 @@ temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]])+\.\./\1 to /g')
 #echo ${weather[1]##*,}
 
 # https://fontawesome.com/icons?s=solid&c=weather
+# echo $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]')
 case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
 "clear" | "sunny")
-    condition="🌞"
+    icon="🌞"
     ;;
 "partly cloudy")
-    condition="⛅"
+    icon="⛅"
     ;;
 "cloudy")
-    condition=""
+    icon=""
     ;;
 "overcast")
-    condition=""
+    icon=""
     ;;
 "mist" | "fog" | "freezing fog")
-    condition="🌁"
+    icon="🌁"
     ;;
 "patchy rain possible" | "patchy light drizzle" | "light drizzle" | "patchy light rain" | "light rain" | "light rain shower" | "rain")
-    condition="🌧"
+    icon="🌧"
     ;;
 "moderate rain at times" | "moderate rain" | "heavy rain at times" | "heavy rain" | "moderate or heavy rain shower" | "torrential rain shower" | "rain shower")
-    condition=""
+    icon=""
     ;;
 "patchy snow possible" | "patchy sleet possible" | "patchy freezing drizzle possible" | "freezing drizzle" | "heavy freezing drizzle" | "light freezing rain" | "moderate or heavy freezing rain" | "light sleet" | "ice pellets" | "light sleet showers" | "moderate or heavy sleet showers")
-    condition=""
+    icon=""
     ;;
 "blowing snow" | "moderate or heavy sleet" | "patchy light snow" | "light snow" | "light snow showers")
-    condition=""
+    icon=""
     ;;
 "blizzard" | "patchy moderate snow" | "moderate snow" | "patchy heavy snow" | "heavy snow" | "moderate or heavy snow with thunder" | "moderate or heavy snow showers")
-    condition=""
+    icon=""
     ;;
 "thundery outbreaks possible" | "patchy light rain with thunder" | "moderate or heavy rain with thunder" | "patchy light snow with thunder")
-    condition="🌩"
+    icon="🌩"
     ;;
 *)
-    condition=""
-    echo -e "{\"text\":\""\<span font=\'RobotoMono Nerd Font\'\>$condition\<\/span\>"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
+    icon=""
+    # echo -e "{\"text\":\""\<span font=\'RobotoMono Nerd Font\'\>$icon\<\/span\>"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
     ;;
 esac
 
-echo -e "{\"text\":\""\<span font=\'RobotoMono Nerd Font\'\>$condition\<\/span\>"  $temperature \", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
+echo -e "{\"text\":\""\<span font=\'RobotoMono Nerd Font\'\>$icon\<\/span\>"  $temperature \", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
