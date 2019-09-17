fc4b3500b37a031664cad3814c3aaae096629a0b
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-09-17 21:04:48 +0100

    Remove redundant apps bin path

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-source /etc/profile.d/apps-bin-path.sh
+#source /etc/profile.d/apps-bin-path.sh
 export PATH=${PATH}:/home/luis/bin
 rofi -m $(expr $(swaymsg -t get_tree | jq '.nodes | map([recurse(.nodes[]?, .floating_nodes[]?) | .focused] | any) | index(true)') - 1) -show drun -show-icons -drun-icon-theme Yaru -run-command 'swaymsg exec -- {cmd}'
+#wofi --show drun --allow-images
