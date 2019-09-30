ce650788651681e7688346c1954af802ba9e4d60
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-09-30 16:12:04 +0100

    Improvements to waybar loading, use wofi-power

---
+++ scripts/wofi-power.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# if rofi >= 1.5.2 "Lock \x00icon\x1ffile-browser," works
+
+entries="Logout\nSuspend\nReboot\nShutdown"
+
+# selected=$(echo $entries | rofi -show-icons -m 0 -dmenu -sep ',' -p "power" -i | awk '{print tolower($1)}')
+selected=$(echo $entries|wofi --width 300 --height 150 --dmenu --cache-file /dev/null | awk '{print tolower($1)}')
+
+case $selected in
+  logout)
+    swaymsg exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
