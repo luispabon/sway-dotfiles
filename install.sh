f03b4457edc8b59c018ad0fa76cad72df3dd8ee7
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2020-08-12 14:28:41 +0100

    Add nvidia power off workaround systemd services to install script

--- install.sh
+++ install.sh
@@ -115,9 +115,11 @@ sudo cp ${current}/assets/ubuntu-wayfire.desktop /usr/share/wayland-sessions/
 
 # Services
 sudo pip3 install undervolt
+sudo cp assets/nvidia-power.service /etc/systemd/system/
 sudo cp assets/undervolt.service /etc/systemd/system/
 sudo cp assets/undervolt.timer /etc/systemd/system/
 sudo systemctl enable undervolt
+sudo systemctl enable nvidia-power
 
 # Disable dell TB16 dock audio autosuspend in tlp
 sudo sed -i 's/#USB_BLACKLIST="1111:2222 3333:4444"/USB_BLACKLIST="0bda:4014"/g' /etc/default/tlp
