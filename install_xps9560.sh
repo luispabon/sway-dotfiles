02af4579b4850cba50963f1f68395b8cf87e7660
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2020-10-28 14:06:15 +0000

    Ensure undervolt timer starts as soon as we install it. Add auto-cpufreq

--- install_xps9560.sh
+++ install_xps9560.sh
@@ -7,6 +7,7 @@ sudo cp assets/undervolt.service /etc/systemd/system/
 sudo cp assets/undervolt.timer /etc/systemd/system/
 sudo systemctl enable undervolt
 sudo systemctl enable undervolt.timer
+sudo systemctl start undervolt.timer
 
 sudo cp assets/xps-powersave.service /etc/systemd/system
 sudo cp assets/xps-powersave-service.sh /usr/bin/
@@ -21,3 +22,7 @@ sudo sed -i 's/#USB_BLACKLIST="1111:2222 3333:4444"/USB_BLACKLIST="0bda:4014"/g'
 # Installing crontabs
 cat assets/crontab-root | sudo crontab -
 cat assets/crontab-luis | crontab -
+
+# auto-cpufreq
+sudo snap install auto-cpufreq
+sudo auto-cpufreq --install
