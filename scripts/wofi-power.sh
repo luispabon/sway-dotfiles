530412ce2e12308faa277ff10a0a4d110be62dfd
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-07-30 22:57:47 +0100

    Simplify power script

--- scripts/wofi-power.sh
+++ scripts/wofi-power.sh
@@ -2,8 +2,7 @@
 
 entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
 
-export LC_ALL=C
-selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | sed 's/[^\x00-\x7F]//g' | awk '{print tolower($1)}')
+selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
 case $selected in
   logout)
