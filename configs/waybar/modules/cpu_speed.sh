f8700e66594d82e4a9a7dcce92e460dd37d9a397
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2022-03-02 17:07:28 +0000

    Fix cpu_speed waybar script for Ubuntu 22.04

--- configs/waybar/modules/cpu_speed.sh
+++ configs/waybar/modules/cpu_speed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 class=cpu_speed
-speed_mhz=$(lscpu | grep "CPU MHz" | sed --expression "s/CPU MHz:[[:space:]]*//g" | xargs printf "%.*f\n" 0)
+speed_mhz=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 
 # speed_ghz=`echo $(($speed_mhz / 1000))`
 
-speed_ghz=`bc -l <<< "$speed_mhz / 1000"`
+speed_ghz=`bc -l <<< "$speed_mhz / 1000000"`
 
 info=$(echo $speed_ghz | xargs printf "%.*f\n" 2)
 
