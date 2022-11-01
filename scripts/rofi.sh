2be45b40d648d656066d5c78c563260b6ae2ccaa
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2022-11-01 15:24:53 +0000

    Tweak rofi script to correctly run apps via swaymsg exec

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #source /etc/profile.d/apps-bin-path.sh
 export PATH=${PATH}:/home/luis/bin
+#rofi -m $(expr $(swaymsg -t get_tree | jq '.nodes | map([recurse(.nodes[]?, .floating_nodes[]?) | .focused] | any) | index(true)') - 1) -show drun -show-icons -drun-icon-theme Yaru -run-command 'swaymsg exec -- {cmd}'
 rofi -m $(expr $(swaymsg -t get_tree | jq '.nodes | map([recurse(.nodes[]?, .floating_nodes[]?) | .focused] | any) | index(true)') - 1) -show drun -show-icons -drun-icon-theme Yaru -run-command 'swaymsg exec -- {cmd}'
 #wofi --show drun --allow-images
