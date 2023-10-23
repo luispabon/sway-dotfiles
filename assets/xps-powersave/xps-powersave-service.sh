27640fc4b11cab44202ff48970333a5e55d8d208
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2023-10-23 10:54:46 +0100

    Comment out SATA link powersaving on xps-powersave service. Don't remember at all why I made this change all the way back in august and I'm a chicken to change it again

--- assets/xps-powersave/xps-powersave-service.sh
+++ assets/xps-powersave/xps-powersave-service.sh
@@ -19,5 +19,5 @@ echo 'auto' > '/sys/bus/pci/devices/0000:00:1d.6/power/control'
 # echo 'auto' > '/sys/bus/pci/devices/0000:00:17.0/ata2/power/control'
 
 # SATA link
-echo 'min_power' > '/sys/class/scsi_host/host0/link_power_management_policy'
-echo 'min_power' > '/sys/class/scsi_host/host1/link_power_management_policy'
+# echo 'min_power' > '/sys/class/scsi_host/host0/link_power_management_policy'
+# echo 'min_power' > '/sys/class/scsi_host/host1/link_power_management_policy'
